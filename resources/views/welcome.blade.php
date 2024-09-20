62b5a076315546eef7ac712f128f1cf37ffe1c96
Franck <franckndi5@gmail.com>
2024-09-20 17:40:52 +0100

    update vite config to see vue interfaces

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo App</title>
     @vite('resources/js/app.js')
+
 </head>
 <body>
     <div id="app"></div>
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        // laravel({
-        //     input: ['resources/css/app.css', 'resources/js/app.js'],
-        //     refresh: true,
-        // }),
-    ],
-    server: {
-       host: true,
-    },
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js'],
+      refresh: true,
+    }),
+    vue(),
+  ],
 });
-
-
